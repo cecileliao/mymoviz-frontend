00b6e0262a20ee6bff2a5130d4df3e2b71cbef3b
Your name <Your email>
2023-04-19 15:35:02 +0200

    debug lien url backend

--- components/Home.js
+++ components/Home.js
@@ -37,7 +37,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-three.vercel.app/movies')
     .then(response => response.json())
     .then(data => {
       setMoviesData(data.movies);
